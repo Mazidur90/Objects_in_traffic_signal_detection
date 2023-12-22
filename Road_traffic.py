c59bc18a7af439289abdc87c7f0391c1a6aa6c42
Mazidur <140770963+Mazidur90@users.noreply.github.com>
2023-12-22 03:39:22 +0100

    Update Road_traffic.py

--- Road_traffic.py
+++ Road_traffic.py
@@ -1,5 +1,22 @@
-import numpy as np
-import pandas as pd
+!pip install roboflow
+!pip install ultralytics
 from ultralytics import YOLO
-model=YOLO('best(1).pt')
-results = model(source=1,show=True,conf=0.3,save=True)
+from IPython import display
+from IPython.display import display,Image
+#display.Clear_output()
+!yolo mode=Checks
+
+from roboflow import Roboflow
+rf = Roboflow(api_key="fNgnxrBW1x0fseS5yZqT")
+project = rf.workspace("lr-tdx").project("road-mark")
+dataset = project.version(3).download("yolov8")
+!yolo task= detect mode=train model=yolov8l.pt data={dataset.location}/data.yaml epochs=20 imgsz=640
+!yolo task= detect mode=val model=yolov8l.pt data={dataset.location}/data.yaml 
+!yolo task= detect mode=predict model=yolov8l.pt data={dataset.location}/data.yaml 
+import glob
+from IPython.display import Image,display
+for image_path in glob.glob (f'/content/runs/detect/predict/*.jpg'):
+      display(Image(filename=image_path,height=600))
+      print("\n")
+  Image(filename=f'/content/runs/detect/train/confusion_matrix.png')
+ Image(filename=f'/content/runs/detect/train/results.png')
